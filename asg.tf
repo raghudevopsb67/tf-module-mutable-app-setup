32d866b103bbe0d9e6fb0225248b2048e945fab5
Raghu K <raghuk.vit@gmail.com>
2022-11-14 07:27:41 +0530

    Update tf-module-mutable-app-setup/

--- asg.tf
+++ asg.tf
@@ -6,6 +6,7 @@ resource "aws_launch_template" "launch-template" {
 
 
 resource "aws_autoscaling_group" "asg" {
+  name                = "${var.env}-${var.name}-asg"
   desired_capacity    = var.min_size
   max_size            = var.max_size
   min_size            = var.min_size
@@ -14,5 +15,11 @@ resource "aws_autoscaling_group" "asg" {
   launch_template {
     id      = aws_launch_template.launch-template.id
     version = "$Latest"
+  }
+
+  tag = {
+    key                 = "Name"
+    value               = "${var.env}-${var.name}"
+    propagate_at_launch = true
   }
 }
