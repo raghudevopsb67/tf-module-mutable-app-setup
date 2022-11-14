9d22e47f22d8f70cb9fba1d1ef5018c5c5812b17
Raghu K <raghuk.vit@gmail.com>
2022-11-14 07:23:46 +0530

    Update tf-module-mutable-app-setup/

--- asg.tf
+++ asg.tf
@@ -6,9 +6,10 @@ resource "aws_launch_template" "launch-template" {
 
 
 resource "aws_autoscaling_group" "asg" {
-  desired_capacity = var.min_size
-  max_size         = var.max_size
-  min_size         = var.min_size
+  desired_capacity    = var.min_size
+  max_size            = var.max_size
+  min_size            = var.min_size
+  vpc_zone_identifier = var.subnets
 
   launch_template {
     id      = aws_launch_template.launch-template.id
