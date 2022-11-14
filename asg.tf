b7df58037de780c398148264cb283116748dd5d6
Raghu K <raghuk.vit@gmail.com>
2022-11-14 07:22:18 +0530

    Update tf-module-mutable-app-setup/

---
+++ asg.tf
@@ -0,0 +1,17 @@
+resource "aws_launch_template" "launch-template" {
+  name_prefix   = "${var.env}-${var.name}-lt"
+  image_id      = data.aws_ami.centos-8-ami.image_id
+  instance_type = var.instance_type
+}
+
+
+resource "aws_autoscaling_group" "asg" {
+  desired_capacity = var.min_size
+  max_size         = var.max_size
+  min_size         = var.min_size
+
+  launch_template {
+    id      = aws_launch_template.launch-template.id
+    version = "$Latest"
+  }
+}
