69ecf2e18804adaebf45dfaf70f7c12cc3a3407f
Raghu K <raghuk.vit@gmail.com>
2022-11-16 06:30:57 +0530

    Update tf-module-mutable-app-setup/

--- ansible-pull.sh
+++ ansible-pull.sh
@@ -3,4 +3,5 @@
 LOG=/opt/user-data-$$.log
 yum install python39-devel -y &>>$LOG
 pip3.9 install ansible botocore boto3 &>>$LOG
-ansible-pull -i localhost, -U https://github.com/raghudevopsb67/roboshop-ansible roboshop.yml -e ROLE_NAME=${COMPONENT} -e ENV=${ENV}
+ansible-pull -i localhost, -U https://github.com/raghudevopsb67/roboshop-ansible roboshop.yml -e ROLE_NAME=${COMPONENT} -e ENV=${ENV} &>>$LOG
+
